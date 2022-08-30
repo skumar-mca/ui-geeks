5d3a9191ae10e348fed3fc740a7b7e93799823b4
Sunil Kumar <skumar.mca2010@gmail.com>
2022-08-30 22:32:48 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.scss";
+
+const App = () => {
+  return (
+    <div className="app-body">
+      <h1>Yet Another Learning Source</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,70 @@
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import path from "path";
+import type { Configuration } from "webpack";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+const devServer: DevServerConfiguration = {
+  static: path.join(__dirname, "build"),
+  compress: true,
+  port: 4001
+};
+
+const config: Configuration = {
+  entry: "./src/index.tsx",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript"
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  devServer,
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false
+      //   eslint: {
+      //     files: "./src/**/*"
+      //   }
+    })
+  ]
+};
+
+export default config;

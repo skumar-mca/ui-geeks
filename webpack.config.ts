569d4cf32d4a86a87af1c4d5bc2d445b984dfa9f
Sunil Kumar <skumar.mca2010@gmail.com>
2022-10-12 23:13:04 +0530

    Enabled optimization

--- webpack.config.ts
+++ webpack.config.ts
@@ -69,6 +69,9 @@ const config: Configuration = {
     maxAssetSize: 512000
   },
   devServer,
+  optimization: {
+    chunkIds: 'deterministic'
+  },
   plugins: [
     new ForkTsCheckerWebpackPlugin({
       async: false

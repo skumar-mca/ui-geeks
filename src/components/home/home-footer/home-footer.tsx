840430ce32f98020d5d2c6f854d57e2fbd117450
Sunil Kumar <skumar.mca2010@gmail.com>
2022-10-23 19:47:01 +0530

    Added footer on all pages

--- src/components/home/home-footer/home-footer.tsx
+++ src/components/home/home-footer/home-footer.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import React, { memo } from 'react';
+import useCurrentPath from '../../../custom-hooks/useCurrentRoute';
 import { AppPrefix } from '../../../util/app-constants';
 import { ImagePaths } from '../../../util/image-path-constants';
 import { SOCIAL_LINKS } from '../../../util/url-constants';
@@ -15,70 +16,85 @@ import {
 } from '../../shared/yals-flex/yals-flex.types';
 import './home-footer.scss';
 
-const HomeFooter = () => {
-  const homeFooter = classNames({ [`${AppPrefix}-home-footer`]: true });
-
+const SocialIcons = () => {
   const iconProps = {
     width: 20,
     height: 20
   };
 
   return (
+    <div className='social-info'>
+      <YalsButton
+        noHighlight
+        className='icon-twitter'
+        target='_blank'
+        href={SOCIAL_LINKS.TWITTER}
+      >
+        <IconTwitter {...iconProps} />
+      </YalsButton>
+
+      <YalsButton
+        noHighlight
+        className='icon-linkedin'
+        target='_blank'
+        href={SOCIAL_LINKS.LINKEDIN}
+      >
+        <IconLinkedin {...iconProps} />
+      </YalsButton>
+
+      <YalsButton
+        noHighlight
+        className='icon-instagram'
+        target='_blank'
+        href={SOCIAL_LINKS.INSTAGRAM}
+      >
+        <IconInstagram {...iconProps} />
+      </YalsButton>
+    </div>
+  );
+};
+const HomeFooter = () => {
+  const homeFooter = classNames({ [`${AppPrefix}-home-footer`]: true });
+
+  const currentPath = useCurrentPath();
+
+  const isHomePage = currentPath === '/';
+
+  return (
     <div className={homeFooter}>
       <YalsFlex
         justifyContent={FlexJustifyContentTypes.SpaceBetween}
         flexDirection={FlexDirectionTypes.Column}
-        className='detail-footer'
+        className={`detail-footer ${isHomePage && 'home-page'}`}
       >
-        <div className='social-info'>
-          <YalsButton
-            noHighlight
-            className='icon-twitter'
-            target='_blank'
-            href={SOCIAL_LINKS.TWITTER}
-          >
-            <IconTwitter {...iconProps} />
-          </YalsButton>
+        {isHomePage && (
+          <>
+            <SocialIcons />
 
-          <YalsButton
-            noHighlight
-            className='icon-linkedin'
-            target='_blank'
-            href={SOCIAL_LINKS.LINKEDIN}
-          >
-            <IconLinkedin {...iconProps} />
-          </YalsButton>
-
-          <YalsButton
-            noHighlight
-            className='icon-instagram'
-            target='_blank'
-            href={SOCIAL_LINKS.INSTAGRAM}
-          >
-            <IconInstagram {...iconProps} />
-          </YalsButton>
-        </div>
-
-        <div className='intro-text'>
-          <div className='intro-text-title'>
-            <B>UI-Geeks is an online learning platform.</B>
-          </div>
-          <I>
-            Learn Core and Advanced Concepts, Blogs, Summary of JavaScript,
-            React, Angular, SCSS, CSS. Content of the website is prepared from
-            multiple resources and follows under MIT License and is free to copy
-            and distribute.
-          </I>
-        </div>
+            <div className='intro-text'>
+              <div className='intro-text-title'>
+                <B>UI-Geeks is an online learning platform.</B>
+              </div>
+              <I>
+                Learn Core and Advanced Concepts, Blogs, Summary of JavaScript,
+                React, Angular, SCSS, CSS. Content of the website is prepared
+                from multiple resources and follows under MIT License and is
+                free to copy and distribute.
+              </I>
+            </div>
+          </>
+        )}
 
         <div className='app-info'>
           <div className='app-name'>
             &#169; {new Date().getFullYear()} UI Geeks | UI Learning Platform.
+            {!isHomePage && <SocialIcons />}
           </div>
-
-          <div className='app-icon'>
-            <img src={ImagePaths.APP_LOGO} />
-          </div>
+          {isHomePage && (
+            <div className='app-icon'>
+              <img src={ImagePaths.APP_LOGO} />
+            </div>
+          )}
         </div>
       </YalsFlex>
     </div>
